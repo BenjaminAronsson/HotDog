0fca1612bd7bd931a2779277b2ac9a9fdd15256f
dev <bene.a99@gmail.com>
2019-04-04 08:37:32 +0200

    adding camera functionality

--- HotDog/ViewController.swift
+++ HotDog/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker =  UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = .camera
+        }
+        else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        
+        imagePicker.allowsEditing = false
+        
     }
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true)
+    }
 
+}
+
+extension ViewController : UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+        imageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController : UINavigationControllerDelegate {
+    
 }
 
